9a37556131a0a4336285c32509efc2c46d19d71f
bw <35764019+byrgazov@users.noreply.github.com>
2021-05-17 19:18:15 +0700

    Update __init__.py
    fix check_same

--- recipe/git/__init__.py
+++ recipe/git/__init__.py
@@ -81,11 +81,11 @@ class GitRecipe(object):
         if os.path.exists(self.repo_path) and os.path.exists(os.path.join(self.repo_path, '.git')):
             os.chdir(self.repo_path)
             try:
-                origin = self.git('remote', ['get-url', 'origin'], quiet=False)
-            except:
+                origin = self.git('remote', ['get-url', 'origin'], quiet=False).rstrip()
+            except Exception:
                 # Git before version 2.7.0
                 origin = self.git('remote', ['-v'], quiet=False)
-                origin = findall('^origin\s*(.*)\s*\(fetch\)$', origin, flags=MULTILINE)[0]
+                origin = findall('^origin\s*(.*)\s*\(fetch\)$', origin, flags=MULTILINE)[0].rstrip()
 
         os.chdir(old_cwd)
         if origin == self.url:
